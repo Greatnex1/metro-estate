57adfe4341900e3acbbe172bf0ac40c503576956
Greatnex1 <noahgreatakoni@gmail.com>
2026-01-19 10:05:59 +0100

    add address  model

---
+++ src/main/java/com/nouah/metroestate/model/entity/Address.java
@@ -0,0 +1,36 @@
+package com.nouah.metroestate.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.envers.Audited;
+
+import java.util.Collection;
+
+@Data
+@Audited
+@Entity
+@Table(name = "address")
+public class Address extends BaseEntity {
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "house_number")
+    private String houseNumber;
+
+    @Column(name = "apartment_number")
+    private String apartmentNumber;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @Column(name = "full_address")
+    private String fullAddress;
+
+    @ManyToMany(mappedBy = "addresses",fetch = FetchType.EAGER)
+    private Collection<UserEntity> userEntities;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "estate_id", referencedColumnName = "estate_id")
+    private Estate estate;
+
+}
