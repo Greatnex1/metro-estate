188bc21321f0fd2046f9a7de95caabe7e169effb
Greatnex1 <noahgreatakoni@gmail.com>
2026-01-23 10:41:42 +0100

    add base response

---
+++ src/main/java/com/nouah/metroestate/dto/response/BaseResponse.java
@@ -0,0 +1,68 @@
+package com.nouah.metroestate.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.ErrorResponse;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseResponse<T> {
+    private String message;
+    @JsonIgnore // Don't serialize HttpStatus to JSON
+    private HttpStatus status;
+    private int statusCode;
+    private String error;
+    private boolean success;
+    private LocalDateTime timestamp;
+    private T data;
+
+    // Success factory methods
+    public static <T> BaseResponse<T> success(T data) {
+        return BaseResponse.<T>builder()
+                .data(data)
+                .status(HttpStatus.OK)
+                .statusCode(HttpStatus.OK.value())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    public static <T> BaseResponse<T> success(T data, String message) {
+        return BaseResponse.<T>builder()
+                .data(data)
+                .message(message)
+                .success(true)
+                .status(HttpStatus.OK)
+                .statusCode(HttpStatus.OK.value())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    // Error factory methods
+    public static <T> BaseResponse<T> error(HttpStatus status, String error) {
+        return BaseResponse.<T>builder()
+                .error(error)
+                .success(false)
+                .status(status)
+                .statusCode(status.value())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    public static <T> BaseResponse<T> error(HttpStatus status, ErrorResponse errorResponse) {
+        return BaseResponse.<T>builder()
+//                .error(errorResponse.getError())
+                .status(status)
+                .success(false)
+                .statusCode(status.value())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+}
