d39cffd918f7d6cbdf76951f57d3f57863f1b330
Greatnex1 <noahgreatakoni@gmail.com>
2026-01-23 10:42:32 +0100

    add customer dto

---
+++ src/main/java/com/nouah/metroestate/dto/CustomerDto.java
@@ -0,0 +1,70 @@
+package com.nouah.metroestate.dto;
+
+import com.nouah.metroestate.model.entity.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+import static com.nouah.metroestate.util.DtoMapper.mapRoleToDtoWithoutPermissionDto;
+
+//import static com.nouah.metroestate.util.DtoMapper.mapRoleToDtoWithoutPermissionDto;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CustomerDto implements Serializable {
+
+    private static final long serialVersionUID = 2345L;
+
+    private String userId;
+    private String tier;
+    private String country;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String phoneNumber;
+    private boolean enabled;
+    private boolean accountLocked;
+    private boolean hasPin;
+    private boolean saveCard;
+    private boolean enablePush;
+    private RoleDto role;
+    private String roleName;
+    //    private RoleDto adminRole;
+    private String profileImage;
+    private String designation;
+    private String landlordId;
+    private String tenantId;
+    private String occupantId;
+    private String estateId;
+
+
+    public CustomerDto(UserEntity customer){
+//        this.userId = customer.getUserId();
+        this.firstName = customer.getFirstName();
+        this.lastName = customer.getLastName();
+        this.emailAddress = customer.getEmail();
+        this.phoneNumber = customer.getPhone();
+//        this.tier = customer.getTier().name();
+        this.country = customer.getCountry();
+        this.hasPin = customer.getPin() != null;
+        this.saveCard = customer.isSaveCard();
+        this.enablePush = customer.isEnablePush();
+        this.profileImage = customer.getProfileImage();
+        this.role = mapRoleToDtoWithoutPermissionDto(customer.getRole());
+        this.roleName = customer.getRole().getName();
+        this.tenantId = customer.getTenantId();
+        this.occupantId = customer.getOccupantId();
+        this.landlordId = customer.getLandlordId();
+        this.designation = customer.getDesignation().toString();
+        this.estateId = customer.getEstateId();
+    }
+
+
+
+}
