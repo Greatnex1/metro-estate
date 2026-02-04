ef766519871f9795c9092f26aeef8d51ec3286ca
Greatnex1 <noahgreatakoni@gmail.com>
2026-02-04 19:02:03 +0100

    add otp repo

---
+++ src/main/java/com/nouah/metroestate/repository/OtpRepository.java
@@ -0,0 +1,8 @@
+package com.nouah.metroestate.repository;
+
+import com.nouah.metroestate.model.entity.Otp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OtpRepository extends JpaRepository<Otp, Long> {
+
+}
