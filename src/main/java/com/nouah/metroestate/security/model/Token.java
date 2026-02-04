bcd89df42458eb118af7e13ffff80c893dd58512
Greatnex1 <noahgreatakoni@gmail.com>
2026-02-04 12:34:08 +0100

    add token

---
+++ src/main/java/com/nouah/metroestate/security/model/Token.java
@@ -0,0 +1,14 @@
+package com.nouah.metroestate.security.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class Token {
+
+    private String accessToken;
+    private String refreshToken;
+}
