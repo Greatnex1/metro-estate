38bc0d6a16cae3c3eb15dff092a1664046bf3813
Greatnex1 <noahgreatakoni@gmail.com>
2026-02-10 17:12:44 +0100

    add main

---
+++ src/test/java/com/nouah/metroestate/MetroEstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nouah.metroestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroEstateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
