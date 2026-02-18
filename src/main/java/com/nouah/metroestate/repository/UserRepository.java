c723f479c2a2eba0df5b4f7a30de47d0f569a1af
Greatnex1 <noahgreatakoni@gmail.com>
2026-02-18 18:48:56 +0100

    add user repo

---
+++ src/main/java/com/nouah/metroestate/repository/UserRepository.java
@@ -0,0 +1,41 @@
+package com.nouah.metroestate.repository;
+
+import com.nouah.metroestate.model.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.history.RevisionRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends RevisionRepository<UserEntity, Long, Integer>, JpaRepository<UserEntity, Long>, JpaSpecificationExecutor<UserEntity> {
+    boolean existsByEmail(String emailAddress);
+    boolean existsByPhone(String phone);
+
+    Optional<UserEntity> findByEmail(String emailAddress);
+
+    Optional<UserEntity> findByPhone(String phoneNumber);
+
+    Optional<UserEntity> findByUserId(String customerId);
+
+    @Query("SELECT u FROM UserEntity u WHERE " +
+            "(u.landlordId = :userId OR u.tenantId = :userId) and u.estateId = :estateId")
+    List<UserEntity> findByLandlordOrTenantIdWhereBothArePresent(@Param("userId") String userId, @Param("estateId") String estateId);
+
+    @Query(value = "SELECT COUNT(*) FROM USER_ENTITY", nativeQuery = true)
+    Long countAllUsers(boolean enabled);
+
+    @Query(value = "SELECT COUNT(*) FROM USER_ENTITY WHERE IS_ENABLED = TRUE", nativeQuery = true)
+    Long countAllActiveUsers();
+
+//    @Query(value = "SELECT COUNT(*) FROM USER_ENTITY WHERE IS_ENABLED = FALSE", nativeQuery = true)
+//    Long countAllInActiveUsers();
+
+    @Query(value = "SELECT COUNT(*) FROM USER_ENTITY WHERE DESIGNATION = :designation", nativeQuery = true)
+    Long countAllUsersByDesignation(@Param("designation") String designation);
+
+
+
+}
