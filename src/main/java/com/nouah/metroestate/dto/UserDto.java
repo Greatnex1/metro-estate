c1ce6b25d93280453fedf9a397927eb45adf239b
Greatnex1 <noahgreatakoni@gmail.com>
2026-03-03 12:19:21 +0100

    add user dto

---
+++ src/main/java/com/nouah/metroestate/dto/UserDto.java
@@ -0,0 +1,40 @@
+package com.nouah.metroestate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    protected String userId;
+    protected String firstName;
+    protected String lastName;
+    protected String middleName;
+    protected String email;
+    protected String phoneNumber;
+    protected String lastLogin;
+    protected boolean expiredPassword;
+    protected String lastPasswordChange;
+    protected boolean enabled;
+    protected Integer loginAttempts;
+    protected String nationality;
+    private String lastPaid;
+    private BigDecimal amountPaid;
+    protected RoleDto role;
+    protected String profileImage;
+    private String shortBio;
+    private String landlordId;
+    private String tenantId;
+    private String designation;
+    private String estate;
+    private List<UserDto> subUsersList;
+    protected UserFinancialDetails userFinancialDetails;
+
+}
+
